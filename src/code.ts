3b0e16c885dc9975f05097bc8e9739bb48d57a9f
Jordan Blakey <jordan.t.blakey@gmail.com>
2018-08-13 20:54:41 -0500

    signaling completion and error catching

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,30 @@
-import { Observable as Rx } from 'rxjs';
+import { Observable } from 'rxjs';
 
-console.log(Rx)
+// Create the observable
+var observable = Observable.create((observer: any) => {
+  try {
+    observer.next('Hey guys!')
+    observer.next('How are you?')
+    observer.complete()
+    observer.next('This will not send')
+
+  }
+  catch (err) {
+    observer.error(err)
+  }
+})
+
+// Subscribe to the observable and pass a function
+observable.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem('Completed')
+)
+
+// Print helper method
+function addItem(val: any) {
+  let node = document.createElement('li')
+  let textnode = document.createTextNode(val)
+  node.appendChild(textnode)
+  document.getElementById('output').appendChild(node)
+}
